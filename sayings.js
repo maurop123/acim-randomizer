648f0e3fbaf8fb1a3561e2651ad719389a45af87
Mauro P <maurop123@gmail.com>
2023-12-05 11:01:57 -0700

    wip chapters and sections

---
+++ sayings.js
@@ -0,0 +1,10 @@
+export default [
+	{
+		quote: 'Would you be hostage to the ego, or host to God?',
+		source: {
+			time: '7:49:39',
+			videoLink: 'https://youtu.be/hke9xRWiVZw',
+		},
+	},
+]
+
---
+++ video_list.js
@@ -0,0 +1,18 @@
+export default [
+	{
+        link: "https://www.youtube.com/embed/5hWJN4J-nyI",
+        time: 60*60*8 + 60*47 + 56
+	},
+	{
+        link: "https://www.youtube.com/embed/hke9xRWiVZw",
+        time: 60*60*9 + 60*3 + 42
+	},
+	{
+        link: "https://www.youtube.com/embed/GBrh2bXrwZg",
+        time: 60*60*10 + 60*49 + 49
+	},
+	{
+        link: "https://www.youtube.com/embed/16TlrcW3eGI",
+        time: 60*60*8 + 60*45 + 16
+	}
+]
